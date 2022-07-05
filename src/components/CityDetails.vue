5bb47fb4da7f5748062d389bd56c48cc2fe9c02f
apietryga <antek.pietryga@gmail.com>
2022-07-05 19:05:49 +0200

    1 file modified, 1 added

---
+++ src/components/CityDetails.vue
@@ -0,0 +1,14 @@
+<template>
+    Hello from CityDetails
+    <div>hey</div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    console.log('Hello FROM CITYDETAILS ');
+  },
+};
+</script>
+
+<style></style>
--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -1,29 +1,66 @@
 <template>
   <h1>WeatherApp</h1>
   <input type="text" placeholder="Wpisz lokalizację" v-model="localeInfo" />
+
+  <CityDetails :values="cityValues" />
 </template>
 
 <script>
+import CityDetails from './CityDetails.vue';
 export default {
+  components: {
+    CityDetails,
+  },
+
   // "domyślne" dane - te ładowane na początku
   data() {
     return {
       localeInfo: 'Chicago', // domyślna lokalizacja
+      cityValues: {},
     };
   },
+
   // "sprawdzaj / patrz" element, którego nazwa jest nazwą funkji
-  watch : {
-    // patrz na zmienną localInfo 
-    localeInfo(newVal){
-      console.log('newData: ', newVal)
-    }
+  watch: {
+    // patrz na zmienną localInfo
+    localeInfo(newVal) {
+      this.getLatLon();
+      // console.log('newData: ', newVal);
+    },
   },
 
-
+  // obiekt przetrzymujący wszystkie funkcje dla komponentu
+  methods: {
+    getLatLon() {
+      fetch(
+        'https://api.openweathermap.org/geo/1.0/direct?q=' +
+          this.localeInfo +
+          '&appid=04d03c358e8933ac6823da54c340c97b'
+      )
+        .then((dt) => dt.json())
+        .then((dt) => {
+          this.getWeather(dt[0].lat, dt[0].lon);
+        });
+    },
+    getWeather(lat, lon) {
+      fetch(
+        'https://api.openweathermap.org/data/2.5/forecast?lat=' +
+          lat +
+          '&lon=' +
+          lon +
+          '&appid=04d03c358e8933ac6823da54c340c97b'
+      )
+        .then((dt) => dt.json())
+        .then((dt) => {
+          this.cityValues = dt.city;
+        });
+    },
+  },
 
   // mouted - funkcja wywołująca się po "zamontowaniu komponentu"
   mounted() {
     // console.log('hello');
+    this.getLatLon();
   },
 };
 </script>

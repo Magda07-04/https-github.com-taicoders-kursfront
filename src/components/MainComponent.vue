2584491f360aa619618e862886cc0f1da978aade
apietryga <antek.pietryga@gmail.com>
2022-07-05 17:46:07 +0200

    1 file modified

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -1,8 +1,32 @@
-<script></script>
-
 <template>
-  <h1>Hello from Main Component</h1>
+  <h1>WeatherApp</h1>
+  <input type="text" placeholder="Wpisz lokalizację" v-model="localeInfo" />
 </template>
+
+<script>
+export default {
+  // "domyślne" dane - te ładowane na początku
+  data() {
+    return {
+      localeInfo: 'Chicago', // domyślna lokalizacja
+    };
+  },
+  // "sprawdzaj / patrz" element, którego nazwa jest nazwą funkji
+  watch : {
+    // patrz na zmienną localInfo 
+    localeInfo(newVal){
+      console.log('newData: ', newVal)
+    }
+  },
+
+
+
+  // mouted - funkcja wywołująca się po "zamontowaniu komponentu"
+  mounted() {
+    // console.log('hello');
+  },
+};
+</script>
 
 <style>
 h1 {

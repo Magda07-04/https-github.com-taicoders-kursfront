f4d822b5b52531da308d49534b6aaa75df82d0a9
apietryga <antek.pietryga@gmail.com>
2022-09-20 16:27:26 +0200

    mobile done

--- maze/script.js
+++ maze/script.js
@@ -63,79 +63,146 @@ for(const wall of map){
 }
 
 // detect mobile or desktop
-let isMobile = true
-// let isMobile = false
+let isMobile = navigator.userAgentData.mobile;
 
 // mechanika gry
+// const game = {
+//   // definiujemy wszystkie aktywne elementy gry
+//   maxTime : 5,
+//   buttons: {
+//     time: document.querySelector('.time'),
+//     start: document.querySelector('.start'),
+//     meta: document.querySelector('.meta'),
+//     walls: document.querySelectorAll('.wall'),
+//   },
+//   // metoda przygotowująca grę
+//   init(){
+//     // przypisz do pola start możliwość kliknięcia i rozpoczęcia gry
+
+//     // if(isMobile){ 'touchstart' } else { 'click' }
+//     // isMobile ? 'touchstart' : 'click'
+//     if(!isMobile){
+//       game.buttons.start.addEventListener(isMobile ? 'touchstart' : 'click', game.start)
+//     }
+//     // game.buttons.start.addEventListener('touchmove', game.test)
+//     // gamePlane.addEventListener('touchmove', game.test)
+//     gamePlane.addEventListener('touchmove', game.test)
+//     // game.buttons.start.addEventListener('touchstart', game.start)
+//     // game.buttons.start.addEventListener('click', game.start )
+//     // game.buttons.start.onclick = function () { game.start() }
+
+
+//     game.time = game.maxTime
+//     game.buttons.time.innerHTML = game.time
+//   },
+//   test(e){
+//     let x = document.elementFromPoint(e.touches[0].clientX, e.touches[0].clientY).classList;
+//     x = x[x.length-1]
+//     if( x == 'wall' || x == 'start' ) { return }
+//     if( x == 'meta' ) { return game.over(true) }
+//     game.over( false )
+//   },
+//   start(e){ // start gry
+//     // zablokuj możliwość rozpoczęcia nowej gry
+//     game.buttons.start.removeEventListener(isMobile ? 'touchstart' : 'click', game.start)
+//     // "nasłuchuj" kursora na polu meta (jeśli się tam pojawi, wywoła 
+//     // zakończenie gry z pozytywnym wynikiem
+//     // game.buttons.meta.addEventListener('mousemove', game.over)
+//     game.buttons.meta.addEventListener(isMobile ? 'touchmove' : 'mousemove', game.over)
+
+//     // jeśli nakierujesz myszkę na gamePlane po starcie, to przegrywasz grę
+//     // gamePlane.addEventListener('mousemove', game.gamePlaneListener)
+//     // gamePlane.addEventListener('mousemove', game.gamePlaneListener)
+//     gamePlane.addEventListener(isMobile ? 'touchmove' : 'mousemove', game.gamePlaneListener)
+//     // wyciągamy jako wall każdą ścianę osobno
+//     for(const wall of game.buttons.walls){
+//       // jeśli Twój kursor jest na klasie .wall, to nie wyzwalaj
+//       // żadnych innych słuchaczy (eventListenerów)
+//       // wall.addEventListener('mousemove', game.wallListener)
+//       wall.addEventListener(isMobile ? 'touchmove' : 'mousemove', game.wallListener)
+//     }
+
+//     game.interval = setInterval(function(){
+//       game.time--
+//       if(game.time < 0) { game.over(false) }
+//       game.buttons.time.innerHTML = game.time
+//     }, 1000)
+
+//   },
+//   wallListener(e){
+//     e.stopPropagation();
+//   },
+//   gamePlaneListener(e){
+//     // console.log(e)
+//     game.over(false)
+//   },
+//   // zakończ grę - wynik zależy od result - może być true - wygrana,
+//   // lub false - przegrana
+//   over(result){
+//     // wyświetl odpowiedni komunikat
+//     if(result){
+//       modal.show('WYGRANA!', 'green')
+//       document.body.style.backgroundColor = "green"
+//     }else{
+//       modal.show('PRZEGRANA!', 'red')
+//       document.body.style.backgroundColor = "red"
+//     }
+//     // zdejmij słuchacza z pola meta (przestajemy nasłuchiwać kursor 
+//     // na polu meta)
+//     // game.buttons.meta.removeEventListener('mousemove', game.over)
+//     game.buttons.meta.removeEventListener(isMobile ? 'touchmove' : 'mousemove', game.over)
+
+//     game.buttons.start.removeEventListener(isMobile ? 'touchstart' : 'click', game.start)
+//     // gamePlane.removeEventListener('mousemove', game.gamePlaneListener)
+//     gamePlane.removeEventListener(isMobile ? 'touchmove' : 'mousemove', game.gamePlaneListener)
+//     for(const wall of game.buttons.walls){
+//       // wall.removeEventListener('mousemove', game.wallListener)
+//       wall.removeEventListener(isMobile ? 'touchmove' : 'mousemove', game.wallListener)
+//     }
+
+//     clearInterval(game.interval)
+
+//     // przygotuj nową grę
+//     game.init()
+//   }
+// }
+
 const game = {
-  // definiujemy wszystkie aktywne elementy gry
   maxTime : 5,
-  buttons: {
-    time: document.querySelector('.time'),
-    start: document.querySelector('.start'),
-    meta: document.querySelector('.meta'),
-    walls: document.querySelectorAll('.wall'),
-  },
-  // metoda przygotowująca grę
   init(){
-    // przypisz do pola start możliwość kliknięcia i rozpoczęcia gry
-
-    // if(isMobile){ 'touchstart' } else { 'click' }
-    // isMobile ? 'touchstart' : 'click'
-    game.buttons.start.addEventListener(isMobile ? 'touchstart' : 'click', game.start)
-    game.buttons.start.addEventListener('touchmove', game.test)
-    // game.buttons.start.addEventListener('touchstart', game.start)
-    // game.buttons.start.addEventListener('click', game.start )
-    // game.buttons.start.onclick = function () { game.start() }
-
-
     game.time = game.maxTime
-    game.buttons.time.innerHTML = game.time
+    gamePlane.querySelector('.time').innerHTML = game.time
+    gamePlane.querySelector('.start').addEventListener(isMobile ? 'touchstart' : 'click', game.start)
   },
-  test(e){
-    console.log(e)
-  },
-  start(e){ // start gry
-    console.log('start', e)
-    // zablokuj możliwość rozpoczęcia nowej gry
-    // game.buttons.start.onclick = ""
-    game.buttons.start.removeEventListener(isMobile ? 'touchstart' : 'click', game.start)
-    
-    // "nasłuchuj" kursora na polu meta (jeśli się tam pojawi, wywoła 
-    // zakończenie gry z pozytywnym wynikiem
-    // game.buttons.meta.addEventListener('mousemove', game.over)
-    game.buttons.meta.addEventListener(isMobile ? 'touchmove' : 'mousemove', game.over)
-
-    // jeśli nakierujesz myszkę na gamePlane po starcie, to przegrywasz grę
-    // gamePlane.addEventListener('mousemove', game.gamePlaneListener)
-    // gamePlane.addEventListener('mousemove', game.gamePlaneListener)
-    gamePlane.addEventListener(isMobile ? 'touchmove' : 'mousemove', game.gamePlaneListener)
-    // wyciągamy jako wall każdą ścianę osobno
-    for(const wall of game.buttons.walls){
-      // jeśli Twój kursor jest na klasie .wall, to nie wyzwalaj
-      // żadnych innych słuchaczy (eventListenerów)
-      // wall.addEventListener('mousemove', game.wallListener)
-      wall.addEventListener(isMobile ? 'touchmove' : 'mousemove', game.wallListener)
-    }
-
+  start(){
+    gamePlane.addEventListener(isMobile ? 'touchend' : 'mouseup', game.release)
+    gamePlane.querySelector('.start').removeEventListener(isMobile ? 'touchstart' : 'click', game.start)
+    gamePlane.addEventListener(isMobile ? 'touchmove' : 'mousemove', game.checkMove)
     game.interval = setInterval(function(){
       game.time--
       if(game.time < 0) { game.over(false) }
-      game.buttons.time.innerHTML = game.time
+      gamePlane.querySelector('.time').innerHTML = game.time
     }, 1000)
-
   },
-  wallListener(e){
-    e.stopPropagation();
+  checkMove(e){
+    let x,y;
+    if(isMobile){
+      x = e.touches[0].clientX
+      y = e.touches[0].clientY
+    }else{
+      x = e.clientX
+      y = e.clientY
+    }
+    let aboveElement = document.elementFromPoint(x, y).classList;
+    aboveElement = aboveElement[aboveElement.length-1]
+    if( aboveElement == 'wall' || aboveElement == 'start' ) { return }
+    if( aboveElement == 'meta' ) { game.over(true); return }
+    game.over( false )
   },
-  gamePlaneListener(e){
-    console.log(e)
+  release(){
     game.over(false)
   },
-  // zakończ grę - wynik zależy od result - może być true - wygrana,
-  // lub false - przegrana
   over(result){
-    // wyświetl odpowiedni komunikat
     if(result){
       modal.show('WYGRANA!', 'green')
       document.body.style.backgroundColor = "green"
@@ -143,24 +210,15 @@ const game = {
       modal.show('PRZEGRANA!', 'red')
       document.body.style.backgroundColor = "red"
     }
-    // zdejmij słuchacza z pola meta (przestajemy nasłuchiwać kursor 
-    // na polu meta)
-    // game.buttons.meta.removeEventListener('mousemove', game.over)
-    game.buttons.meta.removeEventListener(isMobile ? 'touchmove' : 'mousemove', game.over)
-
-    // gamePlane.removeEventListener('mousemove', game.gamePlaneListener)
-    gamePlane.removeEventListener(isMobile ? 'touchmove' : 'mousemove', game.gamePlaneListener)
-    for(const wall of game.buttons.walls){
-      // wall.removeEventListener('mousemove', game.wallListener)
-      wall.removeEventListener(isMobile ? 'touchmove' : 'mousemove', game.wallListener)
-    }
-
+    gamePlane.removeEventListener(isMobile ? 'touchend' : 'mouseup', game.release)
+    gamePlane.removeEventListener(isMobile ? 'touchmove' : 'mousemove', game.checkMove)
     clearInterval(game.interval)
-
-    // przygotuj nową grę
-    game.init()
+    game.init();
   }
 }
+
+
+
 // przygotuj grę
 // ta metoda wywołuje się po każdym odświeżeniu strony
 

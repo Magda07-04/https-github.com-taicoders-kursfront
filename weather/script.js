7d0eed85f10e04d720ac39155d87fdd33c21b04e
apietryga <antek.pietryga@gmail.com>
2022-09-21 18:48:52 +0200

    half .cube

--- weather/script.js
+++ weather/script.js
@@ -21,8 +21,12 @@ function timeConverter(UNIX_timestamp){
   return time;
 }
 
-fetch('https://api.openweathermap.org/data/2.5/forecast?lat=50.2649&lon=19.0238&appid=04d03c358e8933ac6823da54c340c97b&units=metric&lang=pl')
-// fetch('https://api.openweathermap.org/data/2.5/forecast?lat=12.3714&lon=1.5197&appid=04d03c358e8933ac6823da54c340c97b&units=metric&lang=pl')
+let list = []
+
+// fetch('https://api.openweathermap.org/data/2.5/forecast?lat=50.2649&lon=19.0238&appid=04d03c358e8933ac6823da54c340c97b&units=metric&lang=pl')
+fetch('https://api.openweathermap.org/data/2.5/forecast?lat=12.3714&lon=1.5197&appid=04d03c358e8933ac6823da54c340c97b&units=metric&lang=pl')
+// fetch('https://api.openweathermap.org/data/2.5/forecast?lat=23.6345&lon=1.5197&appid=04d03c358e8933ac6823da54c340c97b&units=metric&lang=pl')
+// fetch('https://api.openweathermap.org/data/2.5/forecast?lat=19.4326&lon=99.1332&appid=04d03c358e8933ac6823da54c340c97b&units=metric&lang=pl')
 .then(res => res.json())
 .then(res => {
   // console.log(res)
@@ -32,22 +36,46 @@ fetch('https://api.openweathermap.org/data/2.5/forecast?lat=50.2649&lon=19.0238&
   document.querySelector(".city .sunrise").innerText = timeConverter(res.city.sunrise);
   document.querySelector(".city .sunset").innerText = timeConverter(res.city.sunset);
 
-  makeWeatherCube(res.list[0])
+  list = res.list
+  changeCubeInfo(0)
+  // console.log(res.list[0])
+  // for(const cube of res.list){
+  // makeWeatherCube(res.list[0])
+  // }  
 })
 
 
-const makeWeatherCube = (params) => {
-  console.log(params)
-  const cube = document.createElement('div')
-  cube.className = "cube"
-  cube.innerText = "siemanko"
-  document.querySelector('.content').append(cube)
+// const makeWeatherCube = params => {
+//   console.log(params)
+//   const cube = document.createElement("div")
+//   cube.className = "cube"
+//   cube.innerText = "siemanko"
+//   document.querySelector(".content").append(cube)
 
-  // stwórz nowy div (DOM)
-  const dt_txt = document.createElement("div")
-  // wpisz do niego wartość z API
-  dt_txt.innerText = params.dt_txt;
-  // przypnij go do cube'a
-  cube.append(dt_txt)
+//   // stwórz nowy div (DOM)
+//   const dt_txt = document.createElement("div")
+//   // wpisz do niego wartość z API
+//   dt_txt.innerText = params.dt_txt;
+//   // przypnij go do cube'a
+//   cube.append(dt_txt)
+
+// }
+
+
+
+
+const changeCubeInfo = index => {
+  console.log( "EL: ", list[index])
+  document.querySelector(".cube h3 span").innerHTML = list[index].dt_txt
+  document.querySelector(".cube h3 img").src = 
+  `https://openweathermap.org/img/wn/${list[index].weather[0].icon}.png`
+
+  document.querySelector(".cube h4").innerText = list[index].weather[0].description;
+
+  document.querySelector(".weatherMain h3").innerHTML = list[index].main.feels_like + "°C"
+  
+  document.querySelector(".minMax .min").innerHTML = list[index].main.temp_min + "°C"
+  document.querySelector(".minMax .max").innerText = list[index].main.temp_max + "°C"
+
 
 }
